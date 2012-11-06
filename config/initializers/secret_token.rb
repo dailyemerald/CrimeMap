f32021ac8700ba05feef73f33aa386ba4327aaaa
Ivar Vong <ivarjvong@gmail.com>
2012-11-06 09:05:59 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrimeMap::Application.config.secret_token = 'f3f4215aa49b4efc33e9c303e9cda7cafe59dbe1147d7bb2ae7a6558dcb410cd07a90dccd1a73f0e4820bc1504096805014a8860c0281357047bf68d0d836a7d'
